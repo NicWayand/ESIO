63f9647cd82ea717650916d6598bce006c42d4e6
nicwayand <nicway@gmail.com>
2018-02-15 13:34:00 -0800

    temp

--- scripts/download_scripts/download_NSIDC_0051.sh
+++ scripts/download_scripts/download_NSIDC_0051.sh
@@ -5,15 +5,21 @@ set -x  # Echo all lines executed
 set -e  # Stop on any error
 
 # Source path file
-source ../path_file.sh
+#source ../path_file.sh
 
 # FTP locations of data archives
 data_ftp=ftp://sidads.colorado.edu/DATASETS/nsidc0051_gsfc_nasateam_seaice/final-gsfc/north/daily/
 
 # Make sure the ACF Data environment variable is set
 if [ -z "$NSIDC_0051_DATA_DIR" ]; then
-     	echo "Need to set NSIDC_0051_DATA_DIR"
-	exit 1
+	# Try to source path file
+	echo "trying to source path_file.sh"
+	source ../path_file.sh
+	# Check if its now set
+	if [ -z "$NSIDC_0051_DATA_DIR" ]; then
+		echo "Need to set NSIDC_0051_DATA_DIR"
+		exit 1
+	fi
 fi
 
 mkdir -p $NSIDC_0051_DATA_DIR
