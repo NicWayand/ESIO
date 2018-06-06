c95c413d52d40acae5055802c59ee2ada6d1e787
nicwayand <nicway@gmail.com>
2018-06-05 21:28:29 -0700

    Temp commit: working on refactoring modules

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DISTNAME = 'esio'
+PACKAGES = ['esio']
+DESCRIPTION = "Extended Sea Ice Outlook"
+AUTHOR = 'Nic Wayand'
+AUTHOR_EMAIL = 'nicway@gmail.com'
+URL = 'https://github.com/NicWayand/ESIO'
+LICENSE = 'GPL-3.0'
+PYTHON_REQUIRES = '>=3.5'
+INSTALL_REQUIRES = ['xesmf', 'esmpy', 'xarray', 'numpy', 'scipy', 'cartopy', 'seaborn']
+CLASSIFIERS = [
+    'Development Status :: 1 - Beta',
+    'License :: OSI Approved :: GPL-3.0 License',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Scientific/Engineering',
+]
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name=DISTNAME,
+      version=VERSION,
+      license=LICENSE,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      classifiers=CLASSIFIERS,
+      description=DESCRIPTION,
+      long_description=readme(),
+      python_requires=PYTHON_REQUIRES,
+      install_requires=INSTALL_REQUIRES,
+      url=URL,
+      packages=PACKAGES)
