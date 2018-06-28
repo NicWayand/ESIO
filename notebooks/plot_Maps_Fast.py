da625dd5d98147dca832e1b799351efafa0beaf6
nicwayand <nicway@gmail.com>
2018-06-28 14:24:27 -0700

    MUCH faster mapping plots

--- notebooks/plot_Maps_Fast.py
+++ notebooks/plot_Maps_Fast.py
@@ -48,6 +48,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from esio import EsioData as ed
 from esio import ice_plot
+from esio import import_data
 
 import dask
 #dask.set_options(get=dask.threaded.get)
@@ -59,14 +60,6 @@ sns.set_context("talk", font_scale=.8, rc={"lines.linewidth": 2.5})
 
 
 # In[2]:
-
-
-from dask.distributed import Client
-client = Client()
-client
-
-
-# In[3]:
 
 
 def remove_small_contours(p, thres=10):
@@ -84,12 +77,12 @@ def remove_small_contours(p, thres=10):
                 del(level.get_paths()[kp])  # no remove() for Path objects:(
 
 
-# In[4]:
+# In[3]:
 
 
 # Plotting Info
 runType = 'forecast'
-variables = ['sic', 'hi']
+variables = ['sic']
 metrics_all = {'sic':['anomaly','mean','SIP'], 'hi':['mean']}
 #metrics_all = {'sic':['SIP']}
 updateAll = False
@@ -97,7 +90,7 @@ updateAll = False
 MME_NO = ['hcmr']
 
 
-# In[5]:
+# In[4]:
 
 
 # Define Init Periods here, spaced by 7 days (aprox a week)
@@ -115,7 +108,7 @@ init_slice = init_slice[-Npers:] # Select only the last Npers of periods (weeks)
 init_slice
 
 
-# In[6]:
+# In[5]:
 
 
 # Forecast times to plot
@@ -125,6 +118,14 @@ years = pd.to_timedelta(np.arange(1,2), unit='Y') - np.timedelta64(1, 'D') # nee
 slices = weeks.union(months).union(years).round('1d')
 da_slices = xr.DataArray(slices, dims=('fore_time'))
 da_slices.fore_time.values.astype('timedelta64[D]')
+
+
+# In[6]:
+
+
+# Help conversion between "week/month" period used for figure naming and the actual forecast time delta value
+int_2_days_dict = dict(zip(np.arange(0,da_slices.size), da_slices.values))
+days_2_int_dict = {v: k for k, v in int_2_days_dict.items()}
 
 
 # In[7]:
@@ -146,11 +147,19 @@ mean_1980_2010_sic = xr.open_dataset(os.path.join(E.obs_dir, 'NSIDC_0051', 'agg_
 
 import timeit
 start_time = timeit.default_timer()
-ds_81 = xr.open_mfdataset(E.obs['NSIDC_0081']['sipn_nc']+'/*.nc', concat_dim='time', autoclose=True, parallel=True)#,
+ds_81 = xr.open_mfdataset(E.obs['NSIDC_0081']['sipn_nc']+'_yearly/*.nc', concat_dim='time', autoclose=True, parallel=True)#,
 print(timeit.default_timer() - start_time)
 
 
 # In[9]:
+
+
+from dask.distributed import Client
+client = Client()
+client
+
+
+# In[10]:
 
 
 # print(ds_51.time.min().values, ds_51.time.max().values)
@@ -158,7 +167,7 @@ print(timeit.default_timer() - start_time)
 # print(ds_79.time.min().values, ds_79.time.max().values)
 
 
-# In[10]:
+# In[11]:
 
 
 # Define models to plot
@@ -168,7 +177,7 @@ models_2_plot = [x for x in models_2_plot if E.icePredicted[x]] # Only predictiv
 models_2_plot
 
 
-# In[11]:
+# In[ ]:
 
 
 # Get # of models and setup subplot dims
@@ -184,43 +193,47 @@ for cvar in variables:
     if not os.path.exists(fig_dir):
         os.makedirs(fig_dir)
         
-    # Load in netcdf file that keeps track of plot status
-    status_f = os.path.join(fig_dir, cvar+'_status.nc')
     # Make requested dataArray as specified above
-    ds_status_c = xr.DataArray(np.ones((init_slice.size, da_slices.size))*np.NaN, dims=('init_time','fore_time'), coords={'init_time':init_slice,'fore_time':da_slices}) 
-    ds_status_c.name = 'status'
-    ds_status_c = ds_status_c.to_dataset()
-    print(ds_status_c)
+    ds_status = xr.DataArray(np.ones((init_slice.size, da_slices.size))*np.NaN, dims=('init_time','fore_time'), coords={'init_time':init_slice,'fore_time':da_slices}) 
+    ds_status.name = 'status'
+    ds_status = ds_status.to_dataset()
     
-    # If we already have one load it and use it first
-    if (os.path.isfile(status_f)) & (~updateAll):
-        # Load it
-        ds_status_prev = xr.open_dataset(status_f)
-        # Merge with requested init_slice above
-        ds_status = ds_status_prev.combine_first(ds_status_c) # Uses previous True/False data about plot status, extends to new init_time dim
-    else:
-        ds_status = ds_status_c
+    # Check what plots we already have
+    if ~updateAll:
+        fig_files = glob.glob(os.path.join(fig_dir,'*.png'))
+        for fig_f in fig_files:
+            cit = os.path.basename(fig_f).split('_')[3]
+            cft = int(os.path.basename(fig_f).split('_')[4].split('.')[0])
+            ds_status.status.loc[dict(init_time=cit, fore_time=int_2_days_dict[cft])] = 1
+        
+        print(ds_status.status.values)
     
     # Drop IC/FT we have already plotted (orthoginal only)
-    ds_status = ds_status.where(ds_status.isnull(), drop=True)
+    ds_status = ds_status.where(ds_status.status.sum(dim='fore_time')<ds_status.fore_time.size, drop=True)
     
     print("Starting plots...")
     # For each init_time we haven't plotted yet
-    start_time = timeit.default_timer()
+    start_time_cmod = timeit.default_timer()
     for it in ds_status.init_time.values: 
         print(it)
+        it_start = it-np.timedelta64(Ndays,'D') # Start period for init period (it is end of period)
         
         # For each forecast time we haven't plotted yet
         ft_to_plot = ds_status.sel(init_time=it)
         ft_to_plot = ft_to_plot.where(ft_to_plot.isnull(), drop=True).fore_time
-        for (cs, ft) in enumerate(ft_to_plot.values): 
+        for ft in ft_to_plot.values: 
             print(ft.astype('timedelta64[D]'))
-            cs_str = format(cs, '02')
+            cs_str = format(days_2_int_dict[ft], '02')
             cdoy = pd.to_datetime(it + ft).timetuple().tm_yday
+            it_yr = str(pd.to_datetime(it).year)
+            it_m = str(pd.to_datetime(it).month)
+            
+            # Get datetime64 of valid time start and end
+            valid_start = it_start + ft
+            valid_end = it + ft
             
             # Loop through variable of interest + any metrics (i.e. SIP) based on that
             for metric in metrics_all[cvar]:
-
 
                 # Set up plotting info
                 if cvar=='sic':
@@ -257,11 +270,13 @@ for cvar in variables:
 
                 MME_list = []            
                 # New Plot
+                start_time_plot = timeit.default_timer()
                 (f, axes) = ice_plot.multi_polar_axis(ncols=Nc, nrows=Nr, Nplots=Nmod)
                 
                 # Plot Obs (if available)
                 ax_num = 0
                 if ((it + ft) in ds_81.time.values):
+                    
                     if metric=='mean':
                         da_obs_c = ds_81.sic.sel(time=(it + ft))
                     elif metric=='SIP':
@@ -279,12 +294,12 @@ for cvar in variables:
                                           vmin=c_vmin, vmax=c_vmax)
 
                     # Overlay median ice edge
-                    if metric=='mean':
-                        po = median_ice_fill.isel(time=cdoy).plot.contour(ax=axes[ax_num], x='xm', y='ym', 
-                                                                          colors=('#bc0f60'),
-                                                                          linewidths=[0.5],
-                                                                          levels=[0.5])
-                        remove_small_contours(po, thres=10)
+                    #if metric=='mean':
+                        #po = median_ice_fill.isel(time=cdoy).plot.contour(ax=axes[ax_num], x='xm', y='ym', 
+                                                                          #=('#bc0f60'),
+                                                                          #linewidths=[0.5],
+                                                                          #levels=[0.5])
+                        #remove_small_contours(po, thres=10)
                 else: # When were in the future (or obs are missing)
                     if metric=='anomaly': # Still get climatological mean for model difference
                         da_obs_mean = mean_1980_2010_sic.isel(time=cdoy)
@@ -299,27 +314,31 @@ for cvar in variables:
                     axes[i].set_title(E.model[cmod]['model_label'])
 
                     # Load in Model
-                    model_forecast = os.path.join(E.model[cmod][runType]['sipn_nc'], '*.nc') 
-
+                    # Find only files that have current year and month in filename (speeds up loading)
+                    all_files = os.path.join(E.model[cmod][runType]['sipn_nc'], '*'+it_yr+'*'+it_m+'*.nc') 
+                    
                     # Check we have files 
-                    files = glob.glob(model_forecast)
+                    files = glob.glob(all_files)
                     if not files:
                         #print("Skipping model", cmod, "no forecast files found.")
                         continue # Skip this model
-                    ds_model = xr.open_mfdataset(model_forecast, 
-                                chunks={'fore_time': 1, 'init_time': 1, 'nj': 304, 'ni': 448}, 
+                        
+                    # Load in model    
+                    start_time = timeit.default_timer()
+                    ds_model = xr.open_mfdataset(sorted(files), 
+                                                 chunks={'fore_time': 1, 'init_time': 1, 'nj': 304, 'ni': 448},  
                                                  concat_dim='init_time', autoclose=True, parallel=True)
                     ds_model.rename({'nj':'x', 'ni':'y'}, inplace=True)
+                    print("Loading took  ", (timeit.default_timer() - start_time), " seconds.")
                     
                     # Select init period and fore_time of interest
-                    it_start = it-np.timedelta64(Ndays,'D')
+                    #start_time = timeit.default_timer()
                     ds_model = ds_model.sel(init_time=slice(it_start, it))
                     # Check we found any init_times in range
                     if ds_model.init_time.size==0:
                         print('init_time not found.')
                         continue
-                    # Average over init_times
-                    ds_model = ds_model.mean(dim='init_time')
+                    #print("Selecting init_time took  ", (timeit.default_timer() - start_time), " seconds.")
 
                     # Select var of interest (if available)
                     if cvar in ds_model.variables:
@@ -328,14 +347,28 @@ for cvar in variables:
                     else:
                         print('cvar not found.')
                         continue
-
-                    if ft in ds_model.fore_time.values:
-    #                     print('found fore_time')
-                        ds_model = ds_model.sel(fore_time=ft)
-                    else:
-                        print('fore_time not found.')
-                        continue
                     
+                    #start_time = timeit.default_timer()
+                    # Get Valid time
+                    ds_model = import_data.get_valid_time(ds_model)
+
+                    # Check if we have any valid times in range of target dates
+                    ds_model = ds_model.where((ds_model.valid_time>=valid_start) & (ds_model.valid_time<=valid_end), drop=True) 
+                    if ds_model.fore_time.size == 0:
+                        print("no fore_time found for target period.")
+                        continue
+
+                    # Average over for_time and init_times
+                    ds_model = ds_model.mean(dim=['fore_time','init_time'])
+                    
+#                     if ft in ds_model.fore_time.values:
+#                         ds_model = ds_model.sel(fore_time=ft)
+#                     else:
+#                         print('fore_time not found.')
+#                         continue
+                    #print("Selecting fore_time took ", (timeit.default_timer() - start_time), " seconds.")
+                    
+                    #start_time = timeit.default_timer()
     #                 print("Found data for model ", cmod, ". Plotting...")    
                     if metric=='mean': # Calc ensemble mean
                         ds_model = ds_model.mean(dim='ensemble')
@@ -351,7 +384,7 @@ for cvar in variables:
                         ds_model['lon'] = da_obs_mean.lon
                     else:
                         raise ValueError('metric not implemented')
-
+                    #print("Calc metric took  ", (timeit.default_timer() - start_time), " seconds.")
                      
                     # Build MME
                     if 'ensemble' in ds_model:
@@ -360,20 +393,21 @@ for cvar in variables:
                         MME_list.append(ds_model)
 
                     # Plot
+                    #start_time = timeit.default_timer()
                     p = ds_model.plot.pcolormesh(ax=axes[i], x='lon', y='lat', 
                                           transform=ccrs.PlateCarree(),
                                           add_colorbar=False,
                                           cmap=cmap_c,
                                           vmin=c_vmin, vmax=c_vmax)
-
+                    #print("Plotting took  ", (timeit.default_timer() - start_time), " seconds.")
 
                     # Overlay median ice edge
-                    if metric=='mean':
-                        po = median_ice_fill.isel(time=cdoy).plot.contour(ax=axes[i], x='xm', y='ym', 
-                                                                          colors=('#bc0f60'),
-                                                                          linewidths=[0.5],
-                                                                          levels=[0.5]) #, label='Median ice edge 1981-2010')
-                        remove_small_contours(po, thres=10)
+                    #if metric=='mean':
+                        #po = median_ice_fill.isel(time=cdoy).plot.contour(ax=axes[i], x='xm', y='ym', 
+                                                                          #colors=('#bc0f60'),
+                                                                          #linewidths=[0.5],
+                                                                          #levels=[0.5]) #, label='Median ice edge 1981-2010')
+                        #remove_small_contours(po, thres=10)
 
                     axes[i].set_title(E.model[cmod]['model_label'])
                     
@@ -396,12 +430,12 @@ for cvar in variables:
                                                   add_colorbar=False, 
                                                   cmap=cmap_c,vmin=c_vmin, vmax=c_vmax)
                     # Overlay median ice edge
-                    if metric=='mean':
-                        po = median_ice_fill.isel(time=cdoy).plot.contour(ax=axes[ax_num], x='xm', y='ym', 
-                                                                          colors=('#bc0f60'),
-                                                                          linewidths=[0.5],
-                                                                          levels=[0.5])
-                        remove_small_contours(po, thres=10)
+                    #if metric=='mean':
+                        #po = median_ice_fill.isel(time=cdoy).plot.contour(ax=axes[ax_num], x='xm', y='ym', 
+#                                                                           colors=('#bc0f60'),
+#                                                                           linewidths=[0.5],
+#                                                                           levels=[0.5])
+                        #remove_small_contours(po, thres=10)
                 axes[ax_num].set_title('MME')
 
                 # Make pretty
@@ -420,7 +454,7 @@ for cvar in variables:
                 init_time_1 =  pd.to_datetime(it_start).strftime('%Y-%m-%d')
                 valid_time_2 = pd.to_datetime(it+ft).strftime('%Y-%m-%d')
                 valid_time_1 = pd.to_datetime(it_start+ft).strftime('%Y-%m-%d')
-                plt.suptitle('Initialization Time: '+init_time_1+' to '+init_time_2+'\n Valid Time: '+valid_time_1+' to '+valid_time_2,
+                plt.suptitle('Initialization Time: '+init_time_1+' to '+init_time_2+'\n Valid Time: '+valid_time_1+' to '+valid_time_2+'\n Week '+cs_str,
                              fontsize=15)
                 plt.subplots_adjust(top=0.85)
 
@@ -428,6 +462,7 @@ for cvar in variables:
                 f_out = os.path.join(fig_dir,'panArctic_'+metric+'_'+runType+'_'+init_time_2+'_'+cs_str+'.png')
                 f.savefig(f_out,bbox_inches='tight', dpi=200)
                 print("saved ", f_out)
+                print("Figure took  ", (timeit.default_timer() - start_time_plot)/60, " minutes.")
             
                 # Mem clean up
                 plt.close(f)
@@ -435,26 +470,60 @@ for cvar in variables:
                 ds_MME= None
                 da_obs_c = None
                 da_obs_mean = None
-                
-            # Update plot status file (Done with each it and ft) 
-            ds_status.status.loc[dict(init_time=it, fore_time=ft)] = 1
-            # Update plot status file           
-            ds_status.to_netcdf(status_f)
             
         # Done with current it
-        print("Took ", (timeit.default_timer() - start_time)/60, " minutes.")
-        
-    # Update plot status file           
-    #ds_status.to_netcdf(status_f)
-    ds_status = None
+        print("Took ", (timeit.default_timer() - start_time_cmod)/60, " minutes.")
 
 
 # In[ ]:
 
 
-# # Update json file that keeps track of what plots we have
-# import json
-# with open('data.txt', 'w') as outfile:
-#     json.dump(data, outfile)
+# Make requested dataArray as specified above
+ds_status = xr.DataArray(np.ones((init_slice.size, da_slices.size))*np.NaN, dims=('init_time','fore_time'), coords={'init_time':init_slice,'fore_time':da_slices}) 
+ds_status.name = 'status'
+ds_status = ds_status.to_dataset()
+
+# Check what plots we already have
+if ~updateAll:
+    fig_files = glob.glob(os.path.join(fig_dir,'*.png'))
+    for fig_f in fig_files:
+        cit = os.path.basename(fig_f).split('_')[3]
+        cft = int(os.path.basename(fig_f).split('_')[4].split('.')[0])
+        ds_status.status.loc[dict(init_time=cit, fore_time=int_2_days_dict[cft])] = 1
+
+    print(ds_status.status.values)
+
+
+# In[ ]:
+
+
+# Update json file that keeps track of what plots we have
+import json
+
+init_plotted = ds_status.status.sum(dim='fore_time')
+init_plotted = list(reversed(sorted(init_plotted.where(init_plotted>0, drop=True).init_time.values)))
+
+json_format = [pd.to_datetime(x).strftime('%Y-%m-%d') for x in init_plotted]
+json_dict = [{"date":cd,"label":cd} for cd in json_format]
+
+json_f = '/home/disk/sipn/nicway/public_html/sipn/figures/model/all_model/sic/maps_weekly/plotdates_current.json'
+with open(json_f, 'w') as outfile:
+    json.dump(json_dict, outfile)
+
+
+# In[ ]:
+
+
+# Make into Gifs
+# TODO fig_dir hardcoded to current variable
+import subprocess
+for cit in json_format:
+    subprocess.call(str("/home/disk/sipn/nicway/python/ESIO/scripts/makeGif.sh " + fig_dir + " " + cit), shell=True)
+
+
+# In[ ]:
+
+
+print("Finished plotting panArctic Maps.")
 
 
