ab6a403113829c164c20e8e30256e592cb72df2e
nicwayand <nicway@gmail.com>
2018-06-28 13:29:05 -0700

    added png to gif converter script

---
+++ scripts/makeGif.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+inputDir=$1
+dateinit=$2 # '2018-03-25'
+
+## declare an array variable
+declare -a metrics=("mean" "anomaly" "SIP")
+
+## now loop through the above array
+for cm in "${metrics[@]}"
+do
+	prefix=panArctic_${cm}_forecast_${dateinit}
+	convert -delay 40 ${inputDir}/${prefix}*.png ${inputDir}/${prefix}_99.gif
+done
