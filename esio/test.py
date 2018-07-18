bcfb93469ccec968e765fc3b538ed56ae00959f4
nicwayand <nicway@gmail.com>
2018-07-18 15:16:44 -0700

    add tests

---
+++ esio/test.py
@@ -0,0 +1,41 @@
+from esio import EsioData as ed
+from esio import ice_plot
+from esio import import_data
+from esio import metrics
+
+import xarray as xr
+import numpy as np
+import datetime
+
+E = ed.EsioData.load()
+
+
+# Make test model SIP data
+da_sip = xr.DataArray(np.ones((1,1,1,3)), 
+                      dims=('x','y','valid_time','model'), 
+                      coords={'valid_time':[datetime.datetime(2018,1,1)]})
+
+# Make observed sea ice presence data
+da_obs = xr.DataArray(np.ones((1,1,1)), 
+                      dims=('x','y','valid_time'), 
+                      coords={'valid_time':[datetime.datetime(2018,1,1)]})
+
+def test_BSS(da_sip=da_sip, da_obs=da_obs):
+    
+    # Case Model says SIP=1 and Obs=1, BSS = 0
+    BSS = metrics._BSS(mod=da_sip.isel(model=0), 
+                       obs=da_obs, 
+                       time_dim='valid_time')
+    assert BSS == 0
+    
+    # Case Model says SIP=1 and Obs=1, BSS = 0
+    BSS = metrics._BSS(mod=da_sip.isel(model=0)*0, 
+                       obs=da_obs, 
+                       time_dim='valid_time')
+    assert BSS == 1
+    
+    # Case Model says SIP=1 and Obs=1, BSS = 0
+    BSS = metrics._BSS(mod=da_sip.isel(model=0)*0.5, 
+                       obs=da_obs, 
+                       time_dim='valid_time')
+    assert BSS == (0.5)**2
